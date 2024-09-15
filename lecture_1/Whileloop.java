f3db595d58adc14a962d9182640dbd61da6c2194
Nipun-2004 <nipunvashistha8@gmail.com>
2024-09-15 15:07:13 +0530

    While Loop

--- lecture_1/Whileloop.java
+++ lecture_1/Whileloop.java
@@ -1,16 +1,18 @@
 package lecture_1;
 import java.util.Scanner;
 
-public class Forloop {
+public class Whileloop { 
     public static void main(String[] args) {
         Scanner scanner =new Scanner(System.in);
         System.out.println("Enter a Number");
         int n =scanner.nextInt();
-        int sum =0;
-        int i;
-        for(i=0;i<=n;i++)
-        sum+=i;
-        System.out.println(sum);
+        int i=1;
+        while(i<=n){ 
+            System.out.println(i);
+            i++;
+        }
     }
+    
+
     
 }
