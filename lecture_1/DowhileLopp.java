45100ca4c1cbe8812be4c6af7ee2cfdb61cacca8
Nipun-2004 <nipunvashistha8@gmail.com>
2024-09-15 15:16:35 +0530

    Dowhile Loop

---
+++ lecture_1/DowhileLopp.java
@@ -0,0 +1,11 @@
+package lecture_1;
+
+public class DowhileLopp {
+    public static void main(String[] args) {
+        int i=10;
+        do {
+            System.out.println(1);
+        } while (i<10);
+    }
+    
+}
--- lecture_1/Whileloop.java
+++ lecture_1/Whileloop.java
@@ -11,8 +11,5 @@ public class Whileloop {
             System.out.println(i);
             i++;
         }
-    }
-    
-
-    
+    }   
 }
